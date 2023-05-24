ebd8d1fc922104d2b8dcb4c7399ee658bacc474e
jiyeon.kim <yeon19april@gmail.com>
2023-05-24 19:09:18 +0900

    feat: add genre bias pie graph

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -7,6 +7,9 @@ import { VictoryPie, VictoryLegend } from "victory-native";
 import { Container } from "../styles/layout";
 import { LIGHT_BLACK } from "../styles/colors";
 import ProgressBar from "../components/common/ProgressBar";
+import { useFocusEffect } from "@react-navigation/native";
+import auth from "@react-native-firebase/auth";
+import firestore from "@react-native-firebase/firestore";
 
 const mockData = [
   { name: "History", y: 1 },
@@ -17,32 +20,74 @@ const mockData = [
   { name: "Real Estate", y: 5 },
   { name: "Real Estate", y: 6 },
 ];
+
+type UserTag = {
+  name: string;
+  y: number;
+};
+
 const colorScale = ["tomato", "orange", "gold", "cyan", "navy"];
+
 const Home = () => {
-  // const [data, setData] = useState(null);
-  const screenWidth = Dimensions.get("window").width;
+  const [userTags, setUserTags] = useState<UserTag[]>([]);
+  const [totalAmount, setTotalAmount] = useState<number>(0);
+  useFocusEffect(
+    React.useCallback(() => {
+      const tagsObject: { [tag: string]: number } = {};
+      let amount = 0;
+      const unsubscribe = firestore()
+        .collection("books")
+        .where("uid", "==", auth().currentUser?.uid)
+        .onSnapshot((snapshot) => {
+          const books = snapshot.docs;
+          books.forEach((doc) => {
+            const bookData = doc.data();
+            if (bookData.tags) {
+              bookData.tags.forEach((tag: string) => {
+                if (tagsObject[tag]) {
+                  tagsObject[tag] += 1; // Increment count if the tag already exists
+                } else {
+                  tagsObject[tag] = 1; // Initialize count if the tag is encountered for the first time
+                }
+                amount += 1;
+              });
+            }
+          });
+          const tagsArray = Object.entries(tagsObject).map(([name, count]) => ({
+            name,
+            y: count,
+          }));
+          setUserTags(tagsArray);
+          setTotalAmount(amount);
+        });
+
+      return () => unsubscribe();
+    }, [])
+  );
+
   return (
     <View>
       <Title>Genre bias</Title>
       <BiasContainer>
         <VictoryPie
           colorScale={"qualitative"}
-          data={mockData}
+          data={userTags}
           innerRadius={120}
           labelRadius={90}
           width={250}
           height={250}
-          labels={({ datum }) => `${Math.floor((datum.y / 25) * 100)}%`}
+          labels={({ datum }) =>
+            `${Math.floor((datum.y / totalAmount) * 100)}%`
+          }
           style={{
             labels: { fill: "white", fontSize: 14 },
           }}
         />
         <VictoryLegend
           orientation="horizontal"
-          gutter={20}
-          itemsPerRow={3}
+          itemsPerRow={1}
           colorScale={"qualitative"}
-          data={mockData}
+          data={userTags}
           height={100}
           x={50}
         />
