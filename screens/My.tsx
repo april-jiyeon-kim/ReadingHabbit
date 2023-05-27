9bfd8cdcb0b2087ac24ff802bb102db222fa3f8a
jiyeon.kim <yeon19april@gmail.com>
2023-05-27 17:08:55 +0900

    feat: set menu on setting page

--- screens/My.tsx
+++ screens/My.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { View, Text, TouchableOpacity } from "react-native";
 import auth from "@react-native-firebase/auth";
+import styled from "styled-components/native";
 interface Props {
   navigation: {
     navigate: (screen: string) => void;
@@ -10,12 +11,36 @@ const My: React.FC<Props> = ({ navigation }) => {
   const onLogout = () => {
     auth().signOut().then();
   };
+  const username = auth().currentUser?.displayName || "";
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <TouchableOpacity onPress={onLogout}>
+    <Wrapper style={{ flex: 1, alignItems: "center" }}>
+      <NameText>HELLO, {username.toUpperCase()}</NameText>
+      <MenuItem disabled>
+        <Text>Language settings</Text>
+      </MenuItem>
+      <MenuItem onPress={onLogout}>
         <Text>Logout</Text>
-      </TouchableOpacity>
-    </View>
+      </MenuItem>
+    </Wrapper>
   );
 };
 export default My;
+
+const Wrapper = styled.View`
+  margin: 16px 24px;
+`;
+
+const NameText = styled.Text`
+  font-family: "Pretendard";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 17px;
+`;
+
+const MenuItem = styled.TouchableOpacity`
+  border-bottom-width: 1px;
+  border-bottom-color: #e8e8e8;
+  padding-bottom: 14px;
+  width: 100%;
+`;
