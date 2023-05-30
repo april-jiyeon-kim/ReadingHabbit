479f358f726166ecdfcc524a1587c69d14aaa6ac
jiyeon.kim <yeon19april@gmail.com>
2023-05-30 22:04:25 +0900

    feat: add section title on setting

--- screens/My.tsx
+++ screens/My.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { View, Text, TouchableOpacity } from "react-native";
 import auth from "@react-native-firebase/auth";
 import styled from "styled-components/native";
+import { SubHeaderText } from "../styles/text";
 interface Props {
   navigation: {
     navigate: (screen: string) => void;
@@ -15,6 +16,9 @@ const My: React.FC<Props> = ({ navigation }) => {
   return (
     <Wrapper style={{ flex: 1, alignItems: "center" }}>
       <NameText>HELLO, {username.toUpperCase()}</NameText>
+      <Section>
+        <SubHeaderText>User Setting</SubHeaderText>
+      </Section>
       <MenuItem disabled>
         <Text>Language settings</Text>
       </MenuItem>
@@ -42,6 +46,13 @@ const NameText = styled.Text`
 const MenuItem = styled.TouchableOpacity`
   border-bottom-width: 1px;
   border-bottom-color: #e8e8e8;
-  padding-bottom: 14px;
+  padding: 14px 0;
   width: 100%;
+  justify-content: center;
+`;
+
+const Section = styled.View`
+  justify-content: flex-start;
+  width: 100%;
+  margin: 10px 0;
 `;
--- styles/text.ts
+++ styles/text.ts
@@ -7,3 +7,12 @@ export const HeaderText = styled.Text`
   font-size: 32px;
   line-height: 38px;
 `;
+
+export const SubHeaderText = styled.Text`
+  font-family: "Pretendard";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 17px;
+  color: #797979;
+`;
